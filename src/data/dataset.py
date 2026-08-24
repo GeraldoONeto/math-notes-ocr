fd745f378121a6434fa5910de924812a330de7ba
GeraldoONeto <gneto281105@gmail.com>
2026-08-24 17:23:13 -0300

    feat: preload dataset text files during initialization to optimize training performance

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -9,18 +9,22 @@ from pix2tex.cli import LatexOCR
 
 class CROHMEDataset(Dataset):
     def __init__(self, folder, transform, tokenizer):
+        
         self.folder = folder
         self.transform = transform
         self.tokenizer = tokenizer 
+        self.file_names = []
+        self.texts = []
 
         png_paths = glob.glob(os.path.join(self.folder, '*.png'))
-        self.file_names = []
         
         for path in png_paths:
             name = os.path.basename(path).replace('.png', '')
-
             self.file_names.append(name)
 
+            txt_path = os.path.join(self.folder, name + 'txt')    
+            with open(txt_path, encoding='utf-8') as f:
+                self.texts.append(f.read())
 
     def __len__(self):
         return len(self.file_names)
@@ -28,15 +32,12 @@ class CROHMEDataset(Dataset):
 
     def __getitem__(self, index):
         name = self.file_names[index]
+        text = self.texts[index]
 
         png_path = os.path.join(self.folder, name + '.png')
-        txt_path = os.path.join(self.folder, name + '.txt')
-
         image = Image.open(png_path)
         image = self.transform(image)
 
-        with open(txt_path, encoding='utf-8') as f:
-            text = f.read()
 
         tokens = self.tokenizer.encode(text)
         final_tokens = [1] + tokens + [2]
